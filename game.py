5d206760f60ff395a10c84d5351c810087def491
unknown <fenrique310@gmail.com>
2023-11-01 11:26:59 -0300

    uso de sprite y correccion de como dibujar en la pantalla ademas del uso de la clase heroe para manejar al pj

--- game.py
+++ game.py
@@ -8,76 +8,129 @@ ALTURA = 800
 pygame.init()#Se inicializa pygame
                                 #Altura#Ancho
 screen = pygame.display.set_mode((ALTURA,ANCHO))#La ventana
-pygame.display.set_caption("GAME 0.1")#Titulo que aparece en la ventana
+pygame.display.set_caption("GAME 0.2")#Titulo que aparece en la ventana
 
 background_image_0 = pygame.image.load("Img/parallax_forest_pack/layers/parallax-forest-back-trees.png")
 background_image_1 = pygame.image.load("Img/parallax_forest_pack/layers/parallax-forest-front-trees.png")
 background_image_transform_0 = pygame.transform.scale(background_image_0, (ALTURA,ANCHO))
 background_image_transform_1 = pygame.transform.scale(background_image_1, (ALTURA,ANCHO))
 ##########################################PERSONAJES############################################################
-hit_box_eje_x = 80
-hit_box_eje_y = 465
-is_jumping = False
-jump_count = 10
-#Funcion que se va a encargar de dibujar
-def draw_hit_box():
-                                            #X              #Y    #Ancho#Altura
-    pygame.draw.rect(screen,(255,255,0),(hit_box_eje_x,hit_box_eje_y,20,60))
+# Heroe (Player)
+left = [pygame.image.load("Img/sprite/Walk_Left/L1.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L2.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L3.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L4.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L5.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L6.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L7.png"),
+        pygame.image.load("Img/sprite/Walk_Left/L8.png"),
+        ]
 
-################################################################################################################
-#########################################MOVIMIENTO#####################################################
-def handle_jump():
-    global hit_box_eje_y, is_jumping, jump_count
+right =[pygame.image.load("Img/sprite/Walk_Right/R1.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R2.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R3.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R4.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R5.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R6.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R7.png"),
+        pygame.image.load("Img/sprite/Walk_Right/R8.png"),
+        ]
 
-    if is_jumping:
-        if jump_count >= -10:
-            neg = 1
-            if jump_count < 0:
-                neg = -1
-            hit_box_eje_y -= (jump_count ** 2) * 0.5 * neg
-            jump_count -= 1
+
+class Heroe:
+    def __init__(self, x, y):
+        # Caminar
+        self.x = x
+        self.y = y
+        self.velx = 10
+        self.vely = 10
+        self.face_right = True
+        self.face_left = False
+        self.stepIndex = 0
+        # Salto
+        self.jump = False
+        # Vida
+        self.hitbox = (self.x, self.y, 64, 64)
+##########################################MOVIMIENTO############################################################
+#Esta funcion esta encargada de mover al heroe 
+    def move_hero(self, userInput):
+        if userInput[pygame.K_d] and self.x <= 760:
+            self.x += self.velx
+            self.face_right = True
+            self.face_left = False
+        elif userInput[pygame.K_a] and self.x >= 0:
+            self.x -= self.velx
+            self.face_right = False
+            self.face_left = True
         else:
-            is_jumping = False
-            jump_count = 10
-########################################################################################################
+            self.stepIndex = 0
+#Esta funcion esta encargada de dibujar la hitbox y de los cambios de los sprites
+    def draw(self, win):
+        self.hitbox = (self.x + 5, self.y + 15, 30, 40)
+        pygame.draw.rect(win, (0,0,0), self.hitbox, 1)
+        if self.stepIndex >= 8:
+            self.stepIndex = 0
+        if self.face_left:
+            win.blit(left[self.stepIndex], (self.x, self.y))
+            self.stepIndex += 1
+        if self.face_right:
+            win.blit(right[self.stepIndex], (self.x, self.y))
+            self.stepIndex += 1
+#Esta funcion esta encargada del salto
+    def jump_motion(self, userInput):
+        if userInput[pygame.K_SPACE] and self.jump is False:
+            self.jump = True
+        if self.jump:
+            self.y -= self.vely*4
+            self.vely -= 1
+        if self.vely < -10:
+            self.jump = False
+            self.vely = 10
+#La direccion del salto 
+    def direction(self):
+        if self.face_right:
+            return 1
+        if self.face_left:
+            return -1
+
+    def cooldown(self):
+        if self.cool_down_count >= 20:
+            self.cool_down_count = 0
+        elif self.cool_down_count > 0:
+            self.cool_down_count += 1
+
+#####################################################DIBUJO#########################################################
+#Dibuja en la pantalla
+def draw_game():
+    screen.fill((0, 0, 0))
+    screen.blit(background_image_transform_0, (0, 0))
+    screen.blit(background_image_transform_1, (2, 2))
+    jugador.draw(screen)
+    pygame.time.delay(30)
+    pygame.display.update()
+#Declara al jugador y seta donde se lo dibuja
+                #Y   #X
+jugador = Heroe(250,450)
 
 fps = pygame.time.Clock()
 
 #########################BUCLE PRINCIPAL#############################
 running = True
 while running:
-    
+
     screen.fill((0,0,0))
     fps.tick(60) 
-    
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #Re-dibuja el cuadrado
-        pressed_keys = pygame.key.get_pressed()
-        if not is_jumping:
-            if True in pressed_keys:
-                if pressed_keys [K_a]:
-                    if hit_box_eje_x > 0:
-                        hit_box_eje_x -= 5#se movera a la izquierda
-                        draw_hit_box()
-                if pressed_keys [K_d]:
-                    if hit_box_eje_x < 780:
-                        hit_box_eje_x += 5#se movera a la derecha
-                        draw_hit_box()
-                if pressed_keys[K_SPACE]:
-                    is_jumping = True
-        else:
-            handle_jump()
+        tecla = pygame.key.get_pressed()
 
-    # Dibuja la imagen de fondo en la pantalla
-    screen.blit(background_image_transform_0, (0, 0))
-    screen.blit(background_image_transform_1, (2, 2))
-    
-    #permanece dibujado el cuadrado
-    draw_hit_box()
+    jugador.move_hero(tecla)
+    jugador.jump_motion(tecla)
 
+    #Dibuja en el juego
+    draw_game()
     pygame.display.flip() #Muestra los cambios
 
 while running:
