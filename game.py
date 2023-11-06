cb42d4155a6be2ed39883e1b8040d59d0d8520bb
unknown <fenrique310@gmail.com>
2023-11-06 15:29:04 -0300

    enemigos, animacion de ataque y hitboxes

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame 
 from pygame.locals import K_a,K_d,K_SPACE, K_RETURN
 import sys
+import random
 
 ANCHO = 600
 ALTURA = 800
@@ -36,6 +37,28 @@ right =[pygame.image.load("Img/sprite/Walk_Right/R1.png"),
         pygame.image.load("Img/sprite/Walk_Right/R8.png"),
         ]
 
+attack_left= [pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L1.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L2.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L3.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L4.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L5.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L6.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L7.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_L/Atk_L8.png"),
+        ]
+attack_right= [pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R1.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R2.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R3.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R4.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R5.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R6.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R7.png"),
+        pygame.image.load("Img/sprite/Ataque/Attack_R/Atk_R8.png"),
+        ]
+
+#Enemigo
+enemigo = [pygame.image.load("Img/sprite/Enemy/radioactive monsters not glowing.png")]
+
 
 class Heroe:
     def __init__(self, x, y):
@@ -49,11 +72,14 @@ class Heroe:
         self.stepIndex = 0
         # Salto
         self.jump = False
+        # Ataque
+        self.attack = False
         # Vida
         self.hitbox = (self.x, self.y, 64, 64)
+        
 ##########################################MOVIMIENTO############################################################
-#Esta funcion esta encargada de mover al heroe 
     def move_hero(self, userInput):
+        #Esta funcion esta encargada de mover al heroe 
         if userInput[pygame.K_d] and self.x <= 760:
             self.x += self.movx
             self.face_right = True
@@ -64,20 +90,31 @@ class Heroe:
             self.face_left = True
         else:
             self.stepIndex = 0
-#Esta funcion esta encargada de dibujar la hitbox y de los cambios de los sprites
-    def draw(self, win):
+    def draw(self, win,userInput):
+        #Esta funcion esta encargada de dibujar la hitbox y de los cambios de los sprites
         self.hitbox = (self.x + 5, self.y + 15, 30, 40)
         pygame.draw.rect(win, (0,0,0), self.hitbox, 1)
-        if self.stepIndex >= 8:
-            self.stepIndex = 0
-        if self.face_left:
-            win.blit(left[self.stepIndex], (self.x, self.y))
-            self.stepIndex += 1
-        if self.face_right:
-            win.blit(right[self.stepIndex], (self.x, self.y))
-            self.stepIndex += 1
-#Esta funcion esta encargada del salto
+        
+        if userInput[pygame.K_RETURN]:
+            if self.stepIndex >= 8:
+                self.stepIndex = 0
+            if self.face_left:
+                win.blit(attack_left[self.stepIndex], (self.x, self.y))
+                self.stepIndex += 1
+            if self.face_right:
+                win.blit(attack_right[self.stepIndex], (self.x, self.y))
+                self.stepIndex += 1
+        elif pygame.K_UP:
+            if self.stepIndex >= 8:
+                self.stepIndex = 0
+            if self.face_left:
+                win.blit(left[self.stepIndex], (self.x, self.y))
+                self.stepIndex += 1
+            if self.face_right:
+                win.blit(right[self.stepIndex], (self.x, self.y))
+                self.stepIndex += 1
     def jump_motion(self, userInput):
+        #Esta funcion esta encargada del salto
         if userInput[pygame.K_SPACE] and self.jump is False:
             self.jump = True
         if self.jump:
@@ -86,18 +123,47 @@ class Heroe:
         if self.movy < -10:
             self.jump = False
             self.movy = 10
-#La direccion del salto 
     def direction(self):
+        #La direccion del salto
         if self.face_right:
             return 1
         if self.face_left:
             return -1
+    def slash(self,userInput):
+        #Funcion del ataque
+        if userInput[pygame.K_RETURN] and self.attack == False:
+            self.attack = True
+        if userInput[pygame.K_UP]:
+            self.attack = False
 
-    def cooldown(self):
-        if self.cool_down_count >= 20:
-            self.cool_down_count = 0
-        elif self.cool_down_count > 0:
-            self.cool_down_count += 1
+class Enemigo:
+    def __init__(self, x, y , direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.stepIndex = 0
+        #Vida
+        self.hitbox = (self.x,self.y,64,64)
+        self.images = [pygame.image.load("Img/sprite/Enemy/radioactive monsters not glowing.png")]
+    def step(self):
+        if self.stepIndex >= 1:
+            self.stepIndex = 0
+    def draw(self,win):
+        self.hitbox = (self.x + 5, self.y + 15, 30, 40)
+        pygame.draw.rect(win, (0, 0, 0), self.hitbox, 1)
+        self.step()
+        if self.direction == left:
+            win.blit(self.images[self.stepIndex], (self.x, self.y))
+        if self.direction == right:
+            win.blit(self.images[self.stepIndex], (self.x, self.y))
+    def move(self):
+        if self.direction == left:
+            self.x -= 5
+        if self.direction == right:
+            self.x += 5
+
+    def off_screen(self):
+        return not(self.x >= -10 and self.x <= 770)
 
 #####################################################DIBUJO#########################################################
 #Dibuja en la pantalla
@@ -105,12 +171,16 @@ def draw_game():
     screen.fill((0, 0, 0))
     screen.blit(background_image_transform_0, (0, 0))
     screen.blit(background_image_transform_1, (2, 2))
-    jugador.draw(screen)
+    jugador.draw(screen,tecla)
+    for enemigo in enemigos:
+        monstruo.draw(screen)
     pygame.time.delay(30)
     pygame.display.update()
-#Declara al jugador y seta donde se lo dibuja
+#Declara al jugador y setea donde se lo dibuja
                 #Y   #X
 jugador = Heroe(250,450)
+#Declara una lista para enemigos
+enemigos = []
 
 fps = pygame.time.Clock()
 
@@ -127,14 +197,25 @@ while running:
         tecla = pygame.key.get_pressed()
 
     jugador.move_hero(tecla)
+    jugador.slash(tecla)
     jugador.jump_motion(tecla)
+    
+    #Se dibuja el Enemigo
+    if len(enemigos) == 0:
+        num_rand = random.randint(0,2)
+        if num_rand == 1:
+            monstruo = Enemigo(750, 460, left)
+            enemigos.append(monstruo)
+        if num_rand == 2:
+            monstruo = Enemigo(50, 460, right)
+            enemigos.append(monstruo)
+    for enemigo in enemigos:
+        monstruo.move()
+        if monstruo.off_screen():
+            enemigos.remove(monstruo)
 
-    #Dibuja en el juego
-    draw_game()
+    draw_game()#Dibuja en el juego
     pygame.display.flip() #Muestra los cambios
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit(404) #se usa para indicar si el programa terminó con éxito o con un error (404)
+pygame.quit()
+sys.exit(404) #se usa para indicar si el programa terminó con éxito o con un error (404)
